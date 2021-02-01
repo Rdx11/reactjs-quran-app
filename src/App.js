133ae5e66c0d6863d97380ab246e4b5ede78ed42
imam ikhlasul <imam.ikhlasul@gmail.com>
2021-02-02 06:46:21 +0700

    quran-app-v1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
 import './App.css';
+import Qlogo from './Qlogo.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <section className="glass">
+          <div className="dashboard">
+            <div className="user">
+              <img src={Qlogo} alt="quran logo" className="ava"/>
+            </div>
+            <div className="links">
+              <div className="link">
+                <h1>Quran App</h1>
+              </div>
+            </div>
+          </div>
+          <div className="df-surah">
+            <div className="search">
+              <input type="text"/>
+            </div>
+            <div className="cards">
+              <div className="card">
+                <img src="./images/assassins.png" alt="" />
+                <div className="card-info">
+                  <h2>Al-Fatihah</h2>
+                  <p>Count: 7</p>
+                  <p>Place: Mecca-Makiyah</p>
+                </div>
+              </div>
+              <div className="card">
+                <img src="./images/assassins.png" alt="" />
+                <div className="card-info">
+                  <h2>Al-Fatihah</h2>
+                  <p>Count: 7</p>
+                  <p>Place: Mecca-Makiyah</p>
+                </div>
+              </div>
+              <div className="card">
+                <img src="./images/assassins.png" alt="" />
+                <div className="card-info">
+                  <h2>Al-Fatihah</h2>
+                  <p>Count: 7</p>
+                  <p>Place: Mecca-Makiyah</p>
+                </div>
+              </div>
+              <div className="card">
+                <img src="./images/assassins.png" alt="" />
+                <div className="card-info">
+                  <h2>Al-Fatihah</h2>
+                  <p>Count: 7</p>
+                  <p>Place: Mecca-Makiyah</p>
+                </div>
+              </div>
+              <div className="card">
+                <img src="./images/assassins.png" alt="" />
+                <div className="card-info">
+                  <h2>Al-Fatihah</h2>
+                  <p>Count: 7</p>
+                  <p>Place: Mecca-Makiyah</p>
+                </div>
+              </div>
+              <div className="card">
+                <img src="./images/assassins.png" alt="" />
+                <div className="card-info">
+                  <h2>Al-Fatihah</h2>
+                  <p>Count: 7</p>
+                  <p>Place: Mecca-Makiyah</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
+      <div className="circle1"></div>
+      <div className="circle2"></div>
     </div>
   );
 }
